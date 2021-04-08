c0bad15f174254b5d095117c776f187705ec7f41
SH1222J <sh1222j@gmail.com>
2021-04-08 22:13:28 +0900

    2번문제   *     **    ***   ****  *****

---
+++ HD_SHJ_210408_2/Source.cpp
@@ -0,0 +1,40 @@
+/*
+특별문제
+for문 이용
+}
+1번문제
+*
+**
+***
+****
+*****
+2번문제
+		*
+	   **
+	  ***
+	 ****
+	*****
+3번문제
+  *
+  **
+  *
+  ***
+  *
+  ****
+  *
+  *****
+
+*/
+#include<stdio.h>
+void main() {
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 4; j > i; j--) {
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	fgetc(stdin);
+}
